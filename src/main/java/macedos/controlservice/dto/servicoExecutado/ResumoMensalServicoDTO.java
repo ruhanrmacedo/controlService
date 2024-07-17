a24d702fab73a7cf35cda94e7b92a9407d280bd0
Ruhan Roberto Macedo <ruhan_350@hotmail.com>
2024-07-17 17:13:50 -0300

    Atualizado valorTotal em ResumoMensalServicoDTO

--- src/main/java/macedos/controlservice/dto/servicoExecutado/ResumoMensalServicoDTO.java
+++ src/main/java/macedos/controlservice/dto/servicoExecutado/ResumoMensalServicoDTO.java
@@ -1,4 +1,4 @@
 package macedos.controlservice.dto.servicoExecutado;
 
-public record ResumoMensalServicoDTO(int quantidadeServicos, double valorTotal1, double valorTotal2) {
+public record ResumoMensalServicoDTO(int quantidadeServicos, double valorTotal1, double valorTotal) {
 }
